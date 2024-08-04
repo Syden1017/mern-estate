809cd1d7d782403afe4031d33a118336ca7f3c03
Александр Шустов <sasha.shustov.05@mail.ru>
2024-08-04 12:05:38 +0300

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -16,4 +16,14 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.satus(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
